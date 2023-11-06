6bd46e1bd3960d451f0a35e1fdf0e0d288660d47
David Barreto <david11barreto11@gmail.com>
2023-11-05 19:09:51 -0500

    Creacion del Login

--- Tareas/views.py
+++ Tareas/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
+def login(request):
+    return render(request,'login.html',{
+        'form':UserCreationForm
+    })
